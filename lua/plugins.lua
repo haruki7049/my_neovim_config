629a4f9f570ee6198dcfb0d62e4607dbb134147b
haruki7049 <tontonkirikiri@gmail.com>
2023-07-04 19:47:25 +0900

    added colorschemes in plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,38 @@ return {
   --     require('config/nvim-tree')
   --   end
   -- },
+
+  -- COLORSCHEME
+  -- zephyr colorscheme
+  {
+    "nvimdev/zephyr-nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd('colorscheme zephyr')
+    end
+  },
+
+  -- dracula colorscheme
+  --{
+  --  "Mofiqul/dracula.nvim",
+  --  lazy = false,
+  --  priority = 1000,
+  --  config = function()
+  --    vim.cmd('colorscheme dracula')
+  --  end
+  --},
+
+  -- tokyonight colorscheme
+  --{
+  --  "folke/tokyonight.nvim",
+  --  lazy = false,
+  --  priority = 1000,
+  --  config = function()
+  --    vim.cmd('colorscheme tokyonight')
+  --  end
+  --},
+
   {
     "j-hui/fidget.nvim",
     tag = "legacy",
