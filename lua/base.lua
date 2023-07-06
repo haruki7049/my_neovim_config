b8a4f7fa3d12c542fd7c608160a63e7685e6ebb0
haruki7049 <tontonkirikiri@gmail.com>
2023-07-06 18:52:36 +0900

    added a few comments and disable default colorscheme

--- lua/base.lua
+++ lua/base.lua
@@ -2,11 +2,14 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
+-- SCENERY
 vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.syntax = 'on'
-vim.cmd('colorscheme default')
+-- disable default colorscheme
+--vim.cmd('colorscheme default')
 
+-- ENCODE
 -- USE UTF-8
 vim.scriptencoding = 'utf-8'
 vim.opt.encoding = 'utf-8'
