b095a7ae0de55f0f24cfc94f2f7fa7f3240a829e
haruki7049 <tontonkirikiri@gmail.com>
2023-07-07 07:08:27 +0900

    changed plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -35,34 +35,8 @@ return {
     end
   },
 
-  -- COLORSCHEME
-  --zephyr colorscheme
-  {
-    "nvimdev/zephyr-nvim",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd('colorscheme zephyr')
-    end
-  },
-  --dracula colorscheme
-  --{
-  --  "Mofiqul/dracula.nvim",
-  --  lazy = false,
-  --  priority = 1000,
-  --  config = function()
-  --    vim.cmd('colorscheme dracula')
-  --  end
-  --},
-  --tokyonight colorscheme
-  --{
-  --  "folke/tokyonight.nvim",
-  --  lazy = false,
-  --  priority = 1000,
-  --  config = function()
-  --    vim.cmd('colorscheme tokyonight')
-  --  end
-  --},
+  --colorscheme
+  require("plugins/colorscheme")
 
   -- underbar plugin, barbar.nvim
   --{
