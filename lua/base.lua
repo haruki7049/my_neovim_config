6606503e702abb5186b2f2b69eab22ae33339367
haruki7049 <tontonkirikiri@gmail.com>
2023-07-06 12:13:35 +0900

    added to use utf-8

--- lua/base.lua
+++ lua/base.lua
@@ -6,3 +6,8 @@ vim.opt.termguicolors = true
 vim.opt.number = true
 vim.opt.syntax = 'on'
 vim.cmd('colorscheme default')
+
+-- USE UTF-8
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
