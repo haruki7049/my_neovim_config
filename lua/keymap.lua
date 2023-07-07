46c34eba6af0bcffb1a7ee0e6e3c07c4a5894606
haruki7049 <tontonkirikiri@gmail.com>
2023-07-07 16:31:05 +0900

    fuga

--- lua/keymap.lua
+++
@@ -1,7 +0,0 @@
---sample
---vim.keymap.set()
---api.nvim_set_keymap()
---TODO: to search what difference are these functions
-
-vim.keymap.set('n', '<leader>t', '<cmd>NvimTreeToggle<cr>')
-vim.keymap.set('n', '<leader>MM', '<cmd>Mason<cr>')
