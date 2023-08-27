9d4cbff1608add5608e163ace81bb974ffe2c909
haruki7049 <tontonkirikiri@gmail.com>
2023-08-27 16:20:14 +0900

    reenabled nvim-tree plugin

--- lua/config/nvim-tree.lua
+++ lua/config/nvim-tree.lua
@@ -1,19 +1,14 @@
-require("nvim-tree").setup({
-  sort_by = "name",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-
-  --KEYMAP for nvim-tree
-  vim.keymap.set('n', '<leader>f', '<cmd>NvimTreeToggle<cr>'),
-
-  --disable netrw
-  vim.g.loaded_netrw = 1
-  vim.g.loaded_netrwPlugin = 1
-})
+return {
+  require("nvim-tree").setup({
+    sort_by = "name",
+    view = {
+      width = 30,
+    },
+    renderer = {
+      group_empty = true,
+    },
+    filters = {
+      dotfiles = true,
+    },
+  }),
+}
--- lua/plugins/FileViewer.lua
+++ lua/plugins/FileViewer.lua
@@ -1,10 +1,18 @@
 return {
   -- FILE VIEWER
   --nvim-tree.lua
-  --{
-  --  "nvim-tree/nvim-tree.lua",
-  --  config = function()
-  --    require('config/nvim-tree')
-  --  end
-  --},
+  {
+    "nvim-tree/nvim-tree.lua",
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    init = function()
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      vim.api.nvim_set_keymap("n", "<leader>f", "<cmd>NvimTreeToggle<cr>", {noremap = true});
+    end,
+    config = function()
+      require('config/nvim-tree')
+    end,
+  },
 }
