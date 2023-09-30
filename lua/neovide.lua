dd662550ac5e4dac20402c41a7a2bbd9c946d6c6
haruki7049 <tontonkirikiri@gmail.com>
2023-09-30 10:35:32 +0900

    changed transparency of neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -3,6 +3,6 @@ local font_name = "UDEV Gothic NF:h14"
 if vim.g.neovide then
   vim.o.guifont = font_name
 
-  vim.g.neovide_transparency = 0.8
-  vim.g.transparency = 0.7
+  vim.g.neovide_transparency = 0.98
+  vim.g.transparency = 0.98
 end
